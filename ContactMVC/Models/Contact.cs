581a1052af076e43bd3d0669e2ae422c379f4fbc
MSI\lpack <l.packster@gmail.com>
2016-10-17 07:50:20 -0600

    img, model, views

---
+++ ContactMVC/Models/Contact.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace ContactMVC.Models
+{
+    public class Contact
+    {
+        public int ContactId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Phone { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string picURL { get; set; }
+    }
+    public class ContactDbContext : DbContext
+    {
+        public DbSet<Contact> Contacts { get; set; }
+    }
+
+}
